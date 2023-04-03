aa43b8a3da8ebd14f25bcbe5ae61904a647eb896
Azam Ali <azam02ali@gmail.com>
2023-04-03 15:17:19 +0530

    first commit

---
+++ frontend/build/precache-manifest.63f207dd8e13e504b493601a86690521.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5f89c997b815f70d0f4",
+    "url": "/static/css/main.73c77f39.chunk.css"
+  },
+  {
+    "revision": "a5f89c997b815f70d0f4",
+    "url": "/static/js/main.a5f89c99.chunk.js"
+  },
+  {
+    "revision": "836aff7c05ef998cd731",
+    "url": "/static/js/1.836aff7c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "da8f70cb5568a819c891014ac1ecbbbe",
+    "url": "/index.html"
+  }
+];
